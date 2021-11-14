23134f1bb0741fe9c5745d82c9d9e7fe2e9a5301
RuanSampaio-code <ruansampaio001@gmail.com>
2021-11-14 16:43:34 -0300

    ad

--- aula01/main.js
+++ aula01/main.js
@@ -1,2 +1,6 @@
 const meuCabecalho = document.querySelector('h1');
 meuCabecalho.textContent = 'Ola mundo!';
+
+window.alert('My first menssager') //vai colocar uma msg na janela 
+window.confirm('Está gostando de JS?') // uma janelade de confirmação
+window.prompt('What is your name?') //uma pergunta
