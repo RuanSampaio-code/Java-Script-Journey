d4114c49743dfa298460926e5f7a5e2534678b35
RuanSampaio-code <ruansampaio001@gmail.com>
2022-03-06 22:37:51 -0300

    Update ex012.js

--- aula12/ex012.js
+++ aula12/ex012.js
@@ -1,6 +1,6 @@
 var agora = new Date()
 var hora  = agora.getHours()
-console.log(`agora são exatamente  ${hora} horas.`)
+console.log(`Agora são exatamente  ${hora} horas.`)
 if (hora<12  && hora>8){
     console.log ('bom dia')
 
