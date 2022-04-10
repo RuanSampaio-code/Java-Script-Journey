81afb67a3c1b6c465e32fd7534cb5714016fb012
RuanSampaio-code <ruansampaio001@gmail.com>
2022-04-10 16:49:33 -0300

    Update main.js

--- aula01/main.js
+++ aula01/main.js
@@ -3,4 +3,5 @@ meuCabecalho.textContent = 'Ola mundo!';
 
 window.alert('My first menssager') //vai colocar uma msg na janela 
 window.confirm('Está gostando de JS?') // uma janelade de confirmação
-window.prompt('What is your name?') //uma pergunta
+var nome =  window.prompt('What is your name?') //uma pergunta
+document.write('Prazer' + nome )
